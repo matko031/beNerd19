77199ffcec45d8dd4dd8d503eba6dd738e29e752
matko031 <matkomatic59@gmail.com>
2019-04-20 19:18:54 +0200

    Final commit

--- helloWorld.sh
+++ helloWorld.sh
@@ -5,7 +5,7 @@ messages=(
 "Every time you finish reading the text, click on the image to go to the next part of this amazing guide to my life. I'm ready when you are ;) " 0 letsgo.png
 "My story begins in Osijek, Croatia where I was born 20 years ago" 0 croatia.png
 "Fast forward 16 year later and I am moving to live in Brussels with my family" 0 atomium.png 
-"One more fast forward and it's 2019 and I'm studying computer science and electrical engineering at KU Leuven and am usually nerding out in my free time" 0 cow.jpg
+"One more fast forward and it's 2019 and I'm studying computer science and electrical engineering at KU Leuven and am usually nerding out in my free time" 0 nerding.png
 "In all those flash forwards I skipped one very important part..." 0 forgetting.png
 "JOINING BEST!" 0 joinedBEST1.png
 "I joined BEST last year in October and it was one of the best decisions I had ever made!" 0 joinedBEST2.png 
@@ -13,7 +13,7 @@ messages=(
 "Last week during the RM I have finally received some first hand information about it from the IT delegate of the RM" 0 rm.png
 "It was also the first time I had heard about beNERD which made my inner nerd feel like in heaven" 0 innernerd.png
 "Needless to say, when the call for the beNERD arrived, I immediately decided to apply." 0 application.png
-"As far as my IT experience is concerned, I am far from being a professional, but I love messing around with my pc and solving random IT related problems" 0 cow.jpg
+"As far as my IT experience is concerned, I am far from being a professional, but I love messing around with my pc and solving random IT related problems" 0 itexperience.png
 "I have some experience in pyhton and java, partially through university classes and partially through some small projects on my own" 0 pythonjava.jpg
 "I also had a summer job last year where I worked on a company website where I got familiar with PHP, HTML, CSS and JS " 0 html.png
 "Apart from that, I also worked shortly on a website for a friend using Django" 0 python.png
